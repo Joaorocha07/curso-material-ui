11dcf3fdf3882fc0f235cc27d3dcc11f631a92b2
João Victor <rocha.joao.victor.50@gmail.com>
2023-11-03 10:31:11 -0300

    criando formularios com o unform

--- src/app/dashboard/pessoas/detalhe/components/DetalhesDePessoas.tsx
+++ src/app/dashboard/pessoas/detalhe/components/DetalhesDePessoas.tsx
@@ -2,9 +2,13 @@
 'use client'
 import FerramentasDetalhes 
   from '@/shared/components/ferramentas-detalhes/FerramentasDetalhes'
+import VTextField from '@/shared/forms/VTextField'
 import LayoutBaseDePagina 
   from '@/shared/layouts/LayoutBaseDePagina'
+import { PessoasService } from '@/shared/services/api/pessoas/PessoasService'
+import { Form } from '@unform/web'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function DetalhesDePessoas({
   id,
@@ -13,17 +17,47 @@ export default function DetalhesDePessoas({
  }) {
   const router = useRouter()
 
+  const [loaging, setLoading] = useState(false)
+  const [nome, setNome] = useState('')
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setLoading(true)
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setLoading(false)
+          if (result instanceof Error) {
+            alert(result.message)
+            router.push('/dashboard/pessoas')
+          } else {
+            console.log(result)
+            setNome(result.nomeCompleto)
+          }
+        })
+    }
+  }, [id])
+
   const handleSave = () => {
     console.log('save')
   }
   
-  const handleDelete = () => {
-    console.log('save')
+  const handleDelete = (id: number) => {
+    if(confirm('Realmente deseja apagar?')) {
+      PessoasService.deleteById(id)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message)
+          } else {
+            alert('Registro apagado com sucesso!')
+            router.push('/dashboard/pessoas')
+          }
+        })
+    }
   }
 
   return (
     <LayoutBaseDePagina 
-      titulo='Detalhe de pessoa'
+      titulo={id === ' nova' ? 'Nova pessoa' : nome}
       barraDeFerramentas={
         <FerramentasDetalhes 
           textoBotaoNovo='Nova'
@@ -32,13 +66,19 @@ export default function DetalhesDePessoas({
           mostrarBotaoApagar={id !== 'nova'}
 
           aoClicarEmSalvar={handleSave}
-          aoClicarEmApagar={handleDelete}
+          aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmNovo={() => router.push('/dashboard/pessoas/detalhe/nova')}
           aoClicarEmVoltar={() => router.push('/dashboard/pessoas')}
         />
       }
     >
-      <p>Detalhe de pessoas {id}</p>
+      <Form onSubmit={(dados) => console.log(dados)}>
+        <VTextField 
+          name='nomeCompleto'  
+        />
+
+        <button type='submit'>Submit</button>
+      </Form>
     </LayoutBaseDePagina>
   )
 }
