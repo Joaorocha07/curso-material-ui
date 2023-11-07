a282de5beda3d40e4f1aa33ac39aa02bbc7ba18b
João Victor <rocha.joao.victor.50@gmail.com>
2023-11-07 10:14:22 -0300

    Dashboard completa

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,48 @@
+/* eslint-disable max-len */
 'use client'
-// eslint-disable-next-line max-len
 import FerramentasListagem from '@/shared/components/ferramentas-listagem/FerramentasListagem'
 import LayoutBaseDePagina 
   from '@/shared/layouts/LayoutBaseDePagina'
-import { Box, Card, Grid } from '@mui/material'
+import { CidadesService } from '@/shared/services/api/cidades/CidadesService'
+import { PessoasService } from '@/shared/services/api/pessoas/PessoasService'
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 export default function Dashboard() {
+  const [loadingCidades, setLoadingCidades] = useState(true)
+  const [loadingPessoas, setLoadingPessoas] = useState(true)
+  const [totalCountCidades, setTotalCountCidades] = useState(0)
+  const [totalCountPessoas, setTotalCountPessoas] = useState(0)
+
+  useEffect(() => {
+    setLoadingCidades(true)
+    setLoadingPessoas(true)
+  
+    CidadesService.getAll(1)
+      .then((result) => {
+        setLoadingCidades(false)
+  
+        if(result instanceof Error) {
+          alert(result.message)
+          return
+        } else {
+          console.log(result)
+          setTotalCountCidades(result.totalCount)
+        }
+      })
+    PessoasService.getAll(1)
+      .then((result) => {
+        setLoadingPessoas(false)
+  
+        if(result instanceof Error) {
+          alert(result.message)
+          return
+        } else {
+          console.log(result)
+          setTotalCountPessoas(result.totalCount)
+        }
+      })
+  }, [])
   return (
     <>
       <LayoutBaseDePagina 
@@ -19,28 +56,59 @@ export default function Dashboard() {
         <Box width='100%' display='flex'>
           <Grid container margin={1}>
             <Grid item container spacing={2}>
-
-              <Grid 
-                item 
-                xs={12} 
-                sm={12} 
-                md={6} 
-                lg={4} 
-                xl={2}
-              >
-                <Card>Teste</Card>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant='h5' align='center'>
+                      Total de cidades
+                    </Typography>
+                    <Box 
+                      p={6} 
+                      display='flex' 
+                      justifyContent='center' 
+                      alignItems='center'
+                    >
+                      {!loadingCidades &&(
+                        <Typography variant='h1'>
+                          {totalCountCidades}
+                        </Typography>
+                      )}
+                      {loadingCidades &&(
+                        <Typography variant='body1'>
+                          Carregando...
+                        </Typography>
+                      )}
+                    </Box>
+                  </CardContent>
+                </Card>
               </Grid>
-              <Grid 
-                item 
-                xs={12} 
-                sm={12} 
-                md={6} 
-                lg={4} 
-                xl={2}
-              >
-                <Card>Teste</Card>
-              </Grid>
 
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant='h5' align='center'>
+                      Total de pessoas
+                    </Typography>
+                    <Box 
+                      p={6} 
+                      display='flex' 
+                      justifyContent='center' 
+                      alignItems='center'
+                    >
+                      {!loadingPessoas &&(
+                        <Typography variant='h1'>
+                          {totalCountPessoas}
+                        </Typography>
+                      )}
+                      {loadingPessoas &&(
+                        <Typography variant='body1'>
+                          Carregando...
+                        </Typography>
+                      )}
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
           </Grid>
         </Box>
